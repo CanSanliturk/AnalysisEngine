97ffb3b406d6f53a8050f0f9ab2c5ac2b02393ef
Can Sanliturk <mcansanliturk@gmail.com>
2021-01-03 15:50:36 +0300

    Add armadillo as linear algebra solver option

---
+++ AnalysisEngine/ArmadilloSolver.cpp
@@ -0,0 +1,8 @@
+#include "ArmadilloSolver.h"
+#include <armadillo>
+
+void* ArmadilloSolver::GetResult()
+{
+
+	return nullptr;
+}
---
+++ AnalysisEngine/ArmadilloSolver.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class ArmadilloSolver
+{
+public:
+	void* GetResult();
+};
