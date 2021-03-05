950e71edc22d7b58f3e79472c6312c245be7a303
Can Sanliturk <mcansanliturk@gmail.com>
2021-03-05 23:59:12 +0300

    Fix whitespace

--- AnalysisEngine/XYZPoint.h
+++ AnalysisEngine/XYZPoint.h
@@ -4,31 +4,31 @@
 
 struct XYZPoint
 {
-	double X;
-	double Y;
-	double Z;
+    double X;
+    double Y;
+    double Z;
 
-	XYZPoint(double x, double y, double z)
-	{
-		this->X = x;
-		this->Y = y;
-		this->Z = z;
-	};
+    XYZPoint(double x, double y, double z)
+    {
+        this->X = x;
+        this->Y = y;
+        this->Z = z;
+    };
 
-	XYZPoint()
-	{
-		this->X = 0;
-		this->Y = 0;
-		this->Z = 0;
-	};
+    XYZPoint()
+    {
+        this->X = 0;
+        this->Y = 0;
+        this->Z = 0;
+    };
 
-	~XYZPoint()
-	{
+    ~XYZPoint()
+    {
 
-	};
+    };
 
-	double DistanceTo(XYZPoint pt)
-	{
-		return sqrt(((this->X - pt.X) * (this->X - pt.X)) + ((this->Y - pt.Y) * (this->Y - pt.Y)) + ((this->Z - pt.Z) * (this->Z - pt.Z)));
-	};
+    double DistanceTo(XYZPoint pt)
+    {
+        return sqrt(((this->X - pt.X) * (this->X - pt.X)) + ((this->Y - pt.Y) * (this->Y - pt.Y)) + ((this->Z - pt.Z) * (this->Z - pt.Z)));
+    };
 };
